44643705d9200f365a8b7329686b75838a195572
rangguy <ranggadwi100@gmail.com>
2025-07-11 18:58:50 +0700

    Not Found

---
+++ not_found_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNotFound(t *testing.T) {
+	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, R *http.Request) {
+		fmt.Fprint(w, "Gak Ketemu")
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Gak Ketemu", string(body))
+}
